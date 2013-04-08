b51995f80ed89acd28f66003f8a9ac453fd8e670
Alex Kuiper <alex@nightwhistler.net>
2013-04-08 18:21:09 +0200

    Added owner-email field to access key.

--- grails-app/domain/net/nightwhistler/pageturner/AccessKey.groovy
+++ grails-app/domain/net/nightwhistler/pageturner/AccessKey.groovy
@@ -4,10 +4,13 @@ class AccessKey {
 
     static constraints = {
         expiresOn nullable: true
+        ownerEmail nullable: true
+        keyOwner nullable: true
     }
 
     String keyCode
     String keyOwner
+    String ownerEmail
 
     Date createdOn = new Date()
     Date expiresOn
